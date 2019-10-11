744fd4f4546bb50caeb2f95b2a16048cc4e4750f
elywin <lukeeraelywin@gmail.com>
2019-10-11 17:45:20 +0300

    form

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <head>
 <title>form</title>
-</head>
+</head>git 
 <body>
-    <form method="POST" action="login.php">
+    <FORM method="POST" action="login.php">
         User name:
         <input type="text" name="text1"><br><br>
         User password:
         <input type="password" name="pwd"><br><br>
         <input type="submit" name="submit" value="Submit">
-    </form>
+</FORM>
 </body>
 </html>
